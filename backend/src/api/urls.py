25c981e3e9b450c6f69cdab4f589a8b10e77202a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-08-16 10:36:59 -0400

    Backend Registration endpoint

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from .views import NewsViewSet, TagViewSet
+from .views import NewsViewSet, TagViewSet, RegisterView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'news', NewsViewSet, basename='news')
-router.register(r'tag', TagViewSet, basename='tag')
+router.register(r'tag', TagViewSet, basename='tag') 
 
-urlpatterns = router.urls
-# urlpatterns = [
-#     path('news/', NewsListView.as_view()),
-#     re_path('^single_news/(?P<news_id>.+)/$', NewsFilterIDView.as_view()),
-#     re_path('^news_by_source/(?P<source>.+)/$', NewsFilterSourceView.as_view()),
-#     re_path('^news_by_tag/(?P<tag>.+)/$', NewsFilterTagView.as_view()),
-
-
-# ]
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register_atuh'),
+]
+urlpatterns += router.urls
--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -2,17 +2,31 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from rest_framework import viewsets
 from itertools import chain
-
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+# from rest_framework import generics
 from .models import News, Tag
 from .serializers import NewsSerializer, TagSerializer
 from .filters import NewsFilter, TagFilter
+from rest_framework.decorators import authentication_classes, permission_classes
 
+@authentication_classes([])
+@permission_classes([])
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     filterset_class = TagFilter
 
+@authentication_classes([])
+@permission_classes([])
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
     filterset_class = NewsFilter
+
+@authentication_classes([])
+@permission_classes([])
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    # permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
