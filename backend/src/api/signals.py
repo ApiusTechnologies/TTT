72448ff70e88d87c23485af7f5897967e11fd8ae
Ubuntu <st4f1@TTT.pxwck0333unudgtogq3a5grord.zx.internal.cloudapp.net>
2023-01-27 15:34:26 +0000

    Disabling signals when loading fixtures

--- backend/src/api/signals.py
+++ backend/src/api/signals.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import UserProfile
+from functools import wraps
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    if kwargs.get('raw'):
+        return
     if created:
         UserProfile.objects.create(user=instance)
     instance.userprofile.save()
